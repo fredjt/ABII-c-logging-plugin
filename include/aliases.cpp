4cbdf82b2e930962c17b37303662ebf00578c423
fredjt <61065187+fredjt@users.noreply.github.com>
2025-12-21 20:02:21 -0500

    Add functions from aliases.h

---
+++ include/aliases.cpp
@@ -0,0 +1,116 @@
+//
+// Created by Trent Tanchin on 7/4/24.
+//
+
+#include "aliases.h"
+
+namespace abii
+{
+static void (*real_setaliasent)() noexcept(true) = nullptr;
+
+extern "C" void abii_setaliasent() __THROW
+{
+    OVERRIDE_PREFIX(setaliasent)
+        pre_fmtd_str str = "setaliasent()";
+        abii_args->push_func(new ArgPrinter(str));
+
+        real_setaliasent();
+    OVERRIDE_SUFFIX(setaliasent,)
+    return real_setaliasent();
+}
+
+static void (*real_endaliasent)() noexcept(true) = nullptr;
+
+extern "C" void abii_endaliasent() __THROW
+{
+    OVERRIDE_PREFIX(endaliasent)
+        pre_fmtd_str str = "endaliasent()";
+        abii_args->push_func(new ArgPrinter(str));
+
+        real_endaliasent();
+    OVERRIDE_SUFFIX(endaliasent,)
+    return real_endaliasent();
+}
+
+static aliasent* (*real_getaliasent)() noexcept(true) = nullptr;
+
+extern "C" aliasent* abii_getaliasent() __THROW
+{
+    OVERRIDE_PREFIX(getaliasent)
+        pre_fmtd_str str = "getaliasent()";
+        abii_args->push_func(new ArgPrinter(str));
+
+        auto abii_ret = real_getaliasent();
+
+        abii_args->push_return(new ArgPrinter(abii_ret, "return"));
+    OVERRIDE_SUFFIX(getaliasent, abii_ret)
+    return real_getaliasent();
+}
+
+static int (*real_getaliasent_r)(aliasent*, char*, size_t, aliasent**) noexcept(true) = nullptr;
+
+extern "C" int abii_getaliasent_r(aliasent* result_buf, char* buffer, size_t buflen, aliasent** result) __THROW
+{
+    OVERRIDE_PREFIX(getaliasent_r)
+        pre_fmtd_str str = "getaliasent_r(__result_buf, __buffer, __buflen, __result)";
+        abii_args->push_func(new ArgPrinter(str));
+
+        abii_args->push_arg(new ArgPrinter(result_buf, "__result_buf"));
+
+        auto printer = ArgPrinter(buffer, "__buffer");
+        printer.set_len(buflen);
+        abii_args->push_arg(&printer);
+
+        abii_args->push_arg(new ArgPrinter(buflen, "__buflen"));
+        abii_args->push_arg(new ArgPrinter(result, "__result"));
+
+        auto abii_ret = real_getaliasent_r(result_buf, buffer, buflen, result);
+
+        abii_args->push_return(new ArgPrinter(abii_ret, "return"));
+    OVERRIDE_SUFFIX(getaliasent, abii_ret)
+    return real_getaliasent_r(result_buf, buffer, buflen, result);
+}
+
+static aliasent* (*real_getaliasbyname)(const char*) noexcept(true) = nullptr;
+
+extern "C" aliasent* abii_getaliasbyname(const char* name) __THROW
+{
+    OVERRIDE_PREFIX(getaliasbyname)
+        pre_fmtd_str str = "getaliasbyname(__name)";
+        abii_args->push_func(new ArgPrinter(str));
+
+        abii_args->push_arg(new ArgPrinter(name, "__name"));
+
+        auto abii_ret = real_getaliasbyname(name);
+
+        abii_args->push_return(new ArgPrinter(abii_ret, "return"));
+    OVERRIDE_SUFFIX(getaliasbyname, abii_ret)
+    return real_getaliasbyname(name);
+}
+
+static int (*real_getaliasbyname_r)(const char*, aliasent*, char*, size_t, aliasent**) noexcept(true) = nullptr;
+
+extern "C" int abii_getaliasbyname_r(const char* name, aliasent* result_buf, char* buffer, size_t buflen,
+                                     aliasent** result) __THROW
+{
+    OVERRIDE_PREFIX(getaliasbyname_r)
+        pre_fmtd_str str = "getaliasbyname_r(__name, __result_buf, __buffer, __buflen, __result)";
+        abii_args->push_func(new ArgPrinter(str));
+
+        abii_args->push_arg(new ArgPrinter(name, "__name"));
+        abii_args->push_arg(new ArgPrinter(result_buf, "__result_buf"));
+
+        auto printer = ArgPrinter(buffer, "__buffer");
+        printer.set_len(buflen);
+        abii_args->push_arg(&printer);
+
+        abii_args->push_arg(new ArgPrinter(buflen, "__buflen"));
+        abii_args->push_arg(new ArgPrinter(result, "__result"));
+
+        auto abii_ret = real_getaliasbyname_r(name, result_buf, buffer, buflen, result);
+
+        abii_args->push_return(new ArgPrinter(abii_ret, "return"));
+    OVERRIDE_SUFFIX(getaliasbyname_r, abii_ret)
+    return real_getaliasbyname_r(name, result_buf, buffer, buflen, result);
+}
+}
---
+++ include/aliases.h
@@ -0,0 +1,28 @@
+//
+// Created by Trent Tanchin on 7/4/24.
+//
+
+#ifndef ABII_C_LOGGING_PLUGIN_ALIASES_H
+#define ABII_C_LOGGING_PLUGIN_ALIASES_H
+
+#include <aliases.h>
+#include <abii/libabii.h>
+
+using namespace abii;
+
+template <typename T> requires std::is_same_v<std::remove_cvref_t<T>, aliasent>
+std::ostream& operator<<(std::ostream& os, T&& obj)
+{
+    OVERRIDE_STREAM_PREFIX
+    abii_args->push_arg(new ArgPrinter(obj.alias_name, "alias_name", &os));
+    abii_args->push_arg(new ArgPrinter(obj.alias_members_len, "alias_members_len", &os));
+
+    auto printer = ArgPrinter(obj.alias_members, "alias_members", &os);
+    printer.set_len(obj.alias_members_len);
+    abii_args->push_arg(&printer);
+
+    abii_args->push_arg(new ArgPrinter(obj.alias_local, "alias_local", &os, RECURSE));
+    OVERRIDE_STREAM_SUFFIX
+}
+
+#endif //ABII_C_LOGGING_PLUGIN_ALIASES_H
