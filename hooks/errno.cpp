cacd64737f4bffd88720ac02a66ff8531427bf64
fredjt <61065187+fredjt@users.noreply.github.com>
2025-12-24 16:15:18 -0500

    Fix return value from abii___errno_location

--- hooks/errno.cpp
+++ hooks/errno.cpp
@@ -17,7 +17,7 @@ int* abii___errno_location() __THROW
         pre_fmtd_str str = "__errno_location()";
         abii_args->push_func(new ArgPrinter(str));
 
-        const auto abii_ret = real___errno_location();
+        auto abii_ret = real___errno_location();
 
         auto printer = new ArgPrinter(abii_ret, "return");
         printer->set_enum_printer_with_depth(print_error_enum_entry, *abii_ret, 1);
